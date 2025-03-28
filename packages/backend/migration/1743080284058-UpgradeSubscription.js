9cf4475a12793b75aaecb299163ad9d3ccce56bd
皐月なふ (Nafu Satsuki) <satsuki@nafusoft.dev>
2025-03-28 16:03:19 +0900

    マイグレーションファイル間違ってたの直した。

--- packages/backend/migration/1743080284058-UpgradeSubscription.js
+++ packages/backend/migration/1743080284058-UpgradeSubscription.js
@@ -2,14 +2,14 @@ export class UpgradeSubscription1743080284058 {
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "meta" ADD "subscriptionPricingTable" character varying(128)`);
         await queryRunner.query(`ALTER TABLE "role" ADD "isForSubscriptions" boolean NOT NULL DEFAULT false`);
-				await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "subscriptionPlanId" character varying(128)`);
-				await queryRunner.query(`UPDATE user SET subscriptionPlanId = sp.stripePriceId FROM subscription_plan sp WHERE user.subscriptionPlanId = sp.id`)
+				await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "subscriptionPlanId" TYPE character varying(128) USING "subscriptionPlanId"::character varying(128)`);
+				await queryRunner.query(`UPDATE "user" SET "subscriptionPlanId" = sp."stripePriceId" FROM subscription_plan sp WHERE "subscriptionPlanId" = sp.id`)
 			  await queryRunner.query(`DROP TABLE "subscription_plan"`);
     }
 
     async down(queryRunner) {
 				await queryRunner.query(`CREATE TABLE "subscription_plan" ("id" character varying(32) NOT NULL, "name" character varying(128) NOT NULL, "stripePriceId" character varying(128) NOT NULL, "roleId" character varying(32) NOT NULL, "isArchived" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_01hk5ppx4h56qe96t9dehjzhye" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "subscriptionPlanId" character varying(32)`);
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "subscriptionPlanId" TYPE character varying(32) USING "subscriptionPlanId"::character varying(32)`);
         await queryRunner.query(`ALTER TABLE "role" DROP COLUMN "isForSubscriptions"`);
         await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "subscriptionPricingTable"`);
     }
